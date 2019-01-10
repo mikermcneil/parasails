55464deb7492c9ad810bf862996b0d229b7e5873
Mike McNeil <git+mikermcneil@sailsjs.com>
2019-01-10 16:49:10 -0600

    Prep prerelease

--- dist/parasails.js
+++ dist/parasails.js
@@ -2,7 +2,7 @@
  * parasails.js
  * (lightweight structures for apps with more than one page)
  *
- * v0.9.0-1
+ * v0.9.0-2
  *
  * Copyright 2014-present, Mike McNeil (@mikermcneil)
  * MIT License
